d291719d7c2ce825347f03e1ce1cc66745d433a1
nazeerbshaik <shaiknazeer1995@gmail.com>
2022-10-22 23:37:33 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
-import React from 'react'
-import './App.css'
+import React,{useState} from 'react';
+import video from './video.mp4';
+import './App.css';
 
-const App = () => {
+function App() {
+  const [netincome, setNetincome] = useState('');
+  const [deductions, setDeductions] = useState('');
+  const [income, setIncome] = useState('');
+  const [result, setResult] = useState('');
+
+  let calcTax = (e) => {
+    e.preventDefault();
+
+    if (netincome === 0 || deductions === 0){
+      alert('Please enter valid data')
+    }else{
+       let income = (netincome - deductions)
+       setIncome(income.toFixed(1))
+       
+       if(income > 250000 && income <= 500000){
+        setResult(income *(5/100))
+       }
+
+    }
+  
+  }
+ 
+  
   return (
     <div className='main'>
-      <div className='container'>
-        <form>
-          <div>
-            <label>enter something</label>
-            <input type="number" />
-          </div>
-          <div>
-            <label>enter something</label>
-            <input type="number" />
-          </div>
-          <div>
-            <label>enter something</label>
-            <input type="number" />
-          </div>
-          <div>
-            <label>enter something</label>
-            <input type="number" />
-          </div>
-          
+      <div className="overlay"></div>
+      <video src={video} autoPlay loop muted />
+      <div className='content'>
+        <form className='form-container' onSubmit={calcTax} >
+          <legend>Tax Calculator</legend>
+          <label>Estimated Annual Income </label>
+          <input type="number" value={netincome} onChange={(e) => setNetincome(e.target.value)}></input>
+          <label>Less: Exemptions/Deductions</label>
+          <input type="number" value={deductions} onChange={(e) => setDeductions(e.target.value)}></input>
+          <button className='submit-btn' type='submit'>Submit</button>
+         
         </form>
+        
       </div>
-    </div>
-  )
+    </div>  
+  );
 }
 
-export default App
+export default App;
+
