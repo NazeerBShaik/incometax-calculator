bf2d452065b67df1583b110cd7a66ce32a64a490
nazeerbshaik <shaiknazeer1995@gmail.com>
2022-10-24 00:16:22 +0530

    bg video and media changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-// import video from './video.mp4';
+import video from './video.mp4';
 import './App.css';
 
 function App() {
@@ -11,38 +11,29 @@ function App() {
   let calcTax = (e) => {
     e.preventDefault();
 
-    if (netincome === 0 || deductions === 0){
-      alert('Please enter valid data')
-    }else{
-       let income = (netincome - deductions)
-       setIncome(income.toFixed(1))
-       
-       if(income > 250000 && income <= 500000){
-        setResult(income *(5/100))
-       }
-
-    }
   
   }
- 
-  
   return (
     <div className='main'>
-      <div className="overlay"></div>
-      {/* <video src={video} autoPlay loop muted /> */}
+      <div className="overlay"><h1>Income Tax Calculator</h1></div>
+      
+      <video src={video} autoPlay loop muted />
       <div className='content'>
         <div>
           <form className='form-container' onSubmit={calcTax} >
-            <legend>Tax Calculator</legend>
             <label>Estimated Annual Income </label>
             <input type="number" value={netincome} onChange={(e) => setNetincome(e.target.value)}></input>
             <label>Less: Exemptions/Deductions</label>
             <input type="number" value={deductions} onChange={(e) => setDeductions(e.target.value)}></input>
+            <label>some thing</label>
+            <input type="number" ></input>
             <button className='submit-btn' type='submit'>Submit</button>         
           </form>
         </div>
         <div>
-          <form className='form-container'>
+          <form className='form-container' onClick={(e)=>e.preventDefault()}>
+            <label>some thing</label>
+            <input type="number" ></input>
             <label>some thing</label>
             <input type="number" ></input>
             <label>some thing</label>
