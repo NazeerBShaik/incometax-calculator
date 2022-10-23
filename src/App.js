61317aa19ab4f30660fbff749e62939098497262
nazeerbshaik <shaiknazeer1995@gmail.com>
2022-10-23 20:43:36 +0530

    created an other form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import video from './video.mp4';
+// import video from './video.mp4';
 import './App.css';
 
 function App() {
@@ -29,17 +29,27 @@ function App() {
   return (
     <div className='main'>
       <div className="overlay"></div>
-      <video src={video} autoPlay loop muted />
+      {/* <video src={video} autoPlay loop muted /> */}
       <div className='content'>
-        <form className='form-container' onSubmit={calcTax} >
-          <legend>Tax Calculator</legend>
-          <label>Estimated Annual Income </label>
-          <input type="number" value={netincome} onChange={(e) => setNetincome(e.target.value)}></input>
-          <label>Less: Exemptions/Deductions</label>
-          <input type="number" value={deductions} onChange={(e) => setDeductions(e.target.value)}></input>
-          <button className='submit-btn' type='submit'>Submit</button>
-         
-        </form>
+        <div>
+          <form className='form-container' onSubmit={calcTax} >
+            <legend>Tax Calculator</legend>
+            <label>Estimated Annual Income </label>
+            <input type="number" value={netincome} onChange={(e) => setNetincome(e.target.value)}></input>
+            <label>Less: Exemptions/Deductions</label>
+            <input type="number" value={deductions} onChange={(e) => setDeductions(e.target.value)}></input>
+            <button className='submit-btn' type='submit'>Submit</button>         
+          </form>
+        </div>
+        <div>
+          <form className='form-container'>
+            <label>some thing</label>
+            <input type="number" ></input>
+            <label>some thing</label>
+            <input type="number" ></input>
+            <button className='submit-btn' type='submit'>Submit</button>
+          </form>
+        </div>
         
       </div>
     </div>  
