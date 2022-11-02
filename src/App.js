6bd6b9cc2826dbba77856b5abdfd926f9edf3849
nazeerbshaik <shaiknazeer1995@gmail.com>
2022-11-02 23:52:39 +0530

    logic changed

--- src/App.js
+++ src/App.js
@@ -8,42 +8,38 @@ function App() {
   const [income, setIncome] = useState('');
   const [oldResult, setOldResult] = useState('');
   const [newResult, setNewResult] = useState('');
-
-       
+   
   let calcTaxOld = () => {
     
-  
-    if (netincome === 0 || deductions === 0){
+    if (netincome === '' || deductions === ''){
       alert('Please enter valid data')
-    
-
     }
     else{
       let income = (netincome - deductions)
       setIncome(income.toFixed(2))
         
-    if(income <= 250000){
-      setOldResult(0)
-      console.log(oldResult);
+      if(income <= 250000){
+        setOldResult(0)
+        console.log(oldResult);
 
-    }else if(income > 250000 && income <= 500000){
-      setOldResult(((income-250000)*(5/100))+((income-250000)*(5/100))*(4/100))
-      console.log(oldResult);
+      }else if(income > 250000 && income <= 500000){
+        setOldResult(((income-250000)*(5/100))+((income-250000)*(5/100))*(4/100))
+        console.log(oldResult);
 
-    }else if(income > 500000 && income <= 1000000){
-      setOldResult((12500 + (income-500000)*(20/100))+(12500 + (income-500000)*(20/100))*(4/100))
-      console.log(oldResult);
+      }else if(income > 500000 && income <= 1000000){
+        setOldResult((12500 + (income-500000)*(20/100))+(12500 + (income-500000)*(20/100))*(4/100))
+        console.log(oldResult);
 
-    }else if(income > 1000000){
-      setOldResult((102500 + (income-1000000)*(30/100))+(102500 + (income-1000000)*(30/100))*(4/100))
-      console.log(oldResult);
-    }    
-  }}
+      }else if(income > 1000000){
+        setOldResult((102500 + (income-1000000)*(30/100))+(102500 + (income-1000000)*(30/100))*(4/100))
+        console.log(oldResult);
+      }    
+    }
+  }
 
   let calcTaxNew = () => {
     
-
-    if (netincome === 0 || deductions === 0){
+    if (netincome === '' || deductions === ''){
       alert('Please enter valid data')
     }else{
       let income = (netincome - 0)
@@ -76,19 +72,18 @@ function App() {
       }else if(income > 1500000){
         setNewResult(187500 + (income-1500000)*(30/100)+(187500 + (income-1500000)*(30/100))*(4/100))   
         console.log(newResult);
-
       }
     }
-
-    
   }
   
-  let submit = (e)=>{
+  
+  let submit = async (e)=>{
     e.preventDefault();
-
+     
     calcTaxNew();
     calcTaxOld();
   }
+  
   const f1=()=>setNetincome('');
   const f2=()=>setDeductions('');
   const f3=()=>setIncome('');
@@ -101,29 +96,24 @@ function App() {
       <video src={video} autoPlay loop muted />
       <div className='content'>
         <div>
-          <form className='form-container' onSubmit={
-              submit
-            } >
+          <form className='form-container' onSubmit={submit} >
             <legend>Tax Calculator</legend>
-            <label>Estimated Annual Income </label>
+            <label>Total Annual Income </label>
             <input type="number" value={netincome} onChange={(e) => setNetincome(e.target.value)}></input>
-            <label>Less: Exemptions/Deductions</label>
+            <label>Total: Exemptions/Deductions</label>
             <input type="number" value={deductions} onChange={(e) => setDeductions(e.target.value)}></input>
             <button className='submit-btn' type='submit'>Submit</button>
           </form>
         </div>
         <div className='result'>
-          <h6>Net Tax payable Income :</h6>
+          <h6>Net Tax payable Income</h6>
           <h6>{income}</h6>
-          <h6>Income Tax For Old Slab Rate:</h6>
+          <h6>Income Tax For Old Slab Rate</h6>
           <h6>{oldResult}</h6>
-          <h6>Income Tax For New Slab Rate:</h6>
+          <h6>Income Tax For New Slab Rate</h6>
           <h6>{newResult}</h6>
-          <h6>Tax Benefit in Old Slab : </h6>
-          <h6>{newResult-oldResult}</h6>
           <button className='submit-btn' type='submit' 
           onClick={()=>{f1();f2();f3();f4();f5()}}
-          
           >Reload</button>
         </div>
       </div>
